2486c57cb57b5d53f9c63bfe2102878a9cb1923c
Bilonik <admin@horovitz.dev>
2026-09-01 11:32:05 -0500

    fix(windows): export the generated registrant API

--- windows/presentation_displays_plugin_c_api.cpp
+++ windows/presentation_displays_plugin_c_api.cpp
@@ -1,10 +1,10 @@
-#include "include/presentation_displays/presentation_displays_plugin_c_api.h"
+#include "include/presentation_displays/presentation_displays_plugin.h"
 
 #include <flutter/plugin_registrar_windows.h>
 
 #include "presentation_displays_plugin.h"
 
-void PresentationDisplaysPluginCApiRegisterWithRegistrar(
+void PresentationDisplaysPluginRegisterWithRegistrar(
     FlutterDesktopPluginRegistrarRef registrar) {
   presentation_displays::PresentationDisplaysPlugin::RegisterWithRegistrar(
       flutter::PluginRegistrarManager::GetInstance()
